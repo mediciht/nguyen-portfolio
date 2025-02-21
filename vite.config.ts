1ebdc0f3bd18432255d9a3be56c848478414e7fa
nguyen.tran <nguyen.tran@accenture.com>
2025-02-21 15:10:39 +0700

    deploy github page

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/skillful-tech-portal/',
+  base: '/my-portfolio/',
   server: {
     host: "::",
     port: 8080,
